9461d1abb8d7854decf985583971d3fdf3ef9c4b
Enzo Pagotto <enzovaspagotto@gmail.com>
2025-05-09 19:26:02 -0300

    fix build problem

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+import { useState, Suspense } from 'react'
 import { signIn } from 'next-auth/react'
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
@@ -10,7 +10,8 @@ import { Loader2, LogIn } from 'lucide-react'
 import { toast } from 'sonner'
 import Link from 'next/link'
 import { useSearchParams } from 'next/navigation'
-export default function LoginPage() {
+
+function LoginForm() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [isLoading, setIsLoading] = useState(false)
@@ -39,8 +40,8 @@ export default function LoginPage() {
         })
       }
     } catch (error) {
-      console.error('Erro ao fazer login:', error)
-      toast.error('Erro ao fazer login', {
+      console.error('Error on login', error)
+      toast.error('Error on login, please try again.', {
         style: {
           background: 'red',
           color: 'white'
@@ -52,65 +53,72 @@ export default function LoginPage() {
   }
 
   return (
+    <Card className="w-full max-w-md justify-between h-[22rem]">
+      <CardHeader className="space-y-1">
+        <CardTitle className="text-2xl font-bold text-center">Login</CardTitle>
+        <CardDescription className="text-center">
+          Enter your credentials to access your account
+        </CardDescription>
+      </CardHeader>
+      <form onSubmit={handleLogin} className='h-full justify-between flex flex-col'>
+        <CardContent className="space-y-4">
+          <div className="space-y-2">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              id="email"
+              type="email"
+              placeholder="your@email.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              disabled={isLoading}
+            />
+          </div>
+          <div className="space-y-2">
+            <Label htmlFor="password">Password</Label>
+            <Input
+              id="password"
+              type="password"
+              placeholder="••••••••"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              disabled={isLoading}
+            />
+          </div>
+        </CardContent>
+        <CardFooter className='flex flex-col gap-2 pt-2'>
+          {url.get('error') && <span className='text-red-500 text-sm'>Invalid credentials, please try again.</span>}
+          <Button 
+            type="submit" 
+            className="w-full"
+            disabled={isLoading || email === '' || password === '' || !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)}
+          >
+            {isLoading ? (
+              <>
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                Logging in...
+              </>
+            ) : (
+              <>
+                <LogIn className="mr-2 h-4 w-4" />
+                Enter
+              </>
+            )}
+          </Button>
+          <span className='text-xs'>Don&apos;t have an account? <Link href="/register" className='text-violet-400'>Register</Link></span>
+        </CardFooter>
+      </form>
+    </Card>
+  )
+}
+
+export default function LoginPage() {
+  return (
     <div className="flex min-h-screen items-center justify-center bg-background">
-      <Card className="w-full max-w-md justify-between h-[22rem]">
-        <CardHeader className="space-y-1">
-          <CardTitle className="text-2xl font-bold text-center">Login</CardTitle>
-          <CardDescription className="text-center">
-            Enter your credentials to access your account
-          </CardDescription>
-        </CardHeader>
-        <form onSubmit={handleLogin} className='h-full justify-between flex flex-col'>
-          <CardContent className="space-y-4">
-            <div className="space-y-2">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                type="email"
-                placeholder="your@email.com"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-                disabled={isLoading}
-              />
-            </div>
-            <div className="space-y-2">
-              <Label htmlFor="password">Password</Label>
-              <Input
-                id="password"
-                type="password"
-                placeholder="••••••••"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-                disabled={isLoading}
-              />
-            </div>
-          </CardContent>
-          <CardFooter className='flex flex-col gap-2 pt-2'>
-            {url.get('error') && <span className='text-red-500 text-sm'>Invalid credentials, please try again.</span>}
-            <Button 
-              type="submit" 
-              className="w-full"
-              disabled={isLoading || email === '' || password === '' || !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)}
-            >
-              {isLoading ? (
-                <>
-                  <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-                  Logging in...
-                </>
-              ) : (
-                <>
-                  <LogIn className="mr-2 h-4 w-4" />
-                  Enter
-                </>
-              )}
-            </Button>
-            <span className='text-xs'>Don&apos;t have an account? <Link href="/register" className='text-violet-400'>Register</Link></span>
-            
-          </CardFooter>
-        </form>
-      </Card>
+      <Suspense fallback={<div>Loading...</div>}>
+        <LoginForm />
+      </Suspense>
     </div>
   )
 }
